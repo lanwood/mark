8ea7a88d318cbb120a1e8919783f5b141f663754
lanwood <917375123@qq.com>
2021-06-06 01:08:02 +0800

    update

--- programming/text_process.py
+++ programming/text_process.py
@@ -12,10 +12,13 @@ python -c "import pstats; p=pstats.Stats('text_process.out'); p.sort_stats('time
 
 python -m http.server
 """
+from typing import List
 
 import pandas as pd
 from tqdm import tqdm
 from collections import Counter
+import re
+import multiprocessing as mp
 
 
 def remove_non_char(need_clean):
@@ -23,6 +26,20 @@ def remove_non_char(need_clean):
     for c in non_char:
         need_clean = need_clean.replace(c, ' ')
     return need_clean
+
+
+non_char = re.compile('[,，\.。【】\[\]\*、\|:;"\'!！\? ？]')
+
+
+def remove_non_char_re(need_clean):
+    return non_char.sub(' ', need_clean)
+
+
+pattern = re.compile('[\(\（\{].*?[\)\）\}]')
+
+
+def remove_all_brackets_re(need_clean):
+    return pattern.sub('', need_clean)
 
 
 def remove_all_brackets(need_clean):
@@ -49,7 +66,7 @@ def remove_all_brackets(need_clean):
 
 def rank_length(text):
     ranked = sorted(text, key=len)  # n * log(n)
-    return ranked[int(len(text) * 0.1) : int(len(text) * 0.9)]
+    return ranked[int(len(text) * 0.1): int(len(text) * 0.9)]
 
 
 def counter_to_text(counter):
@@ -60,7 +77,46 @@ def counter_to_text(counter):
     return result
 
 
+def counter_to_text_v2(counter):
+    return str(dict(counter))
+
+
+def reduce_fn(functions: List, init):
+    if not functions: return init
+    return reduce_fn(functions[1:], functions[0](init))
+
+
+def clean_by_funcs(functions):
+    def _wrap(s):
+        return reduce_fn(functions, s)
+    return _wrap
+
+
+def process_chunk(chunk, index):
+    news = chunk
+    news = news.fillna('')
+
+    functions = [
+        remove_all_brackets_re,
+        remove_non_char
+    ]
+
+    clean = clean_by_funcs(functions)
+    news['content'].apply(lambda s: clean(s))
+
+    all_length_medium = rank_length(news['content'].tolist())
+
+    print(all_length_medium[:10])
+
+    with open('words_count_{}.out'.format(index), 'w', encoding='gb18030') as f:
+        for text in all_length_medium:
+            counter = Counter(text)
+            f.write('\n')
+            f.write(counter_to_text_v2(counter)) # v3
+
+
 if __name__ == '__main__':
+    CHUNK_SIZE = 15000
 
     test_string = """
         知乎答主@小司（北京市政设计研究总院工程师）解释道，觉得火车声音大是人离得近。
@@ -70,27 +126,55 @@ if __name__ == '__main__':
         当我们能听到火车的噪声时，一般距离我们只有不足200米了，此时，火车大概6~7秒就可以到达人的面前。
         反应稍微慢点，是很可能跑不出铁轨范围的!
     """
-    print(remove_non_char(test_string))
-    print(remove_all_brackets(test_string))
+    print(remove_non_char_re(test_string))
+    print(remove_all_brackets_re(test_string))
 
-    # exit(0)
+    exit(0)
 
-    news = pd.read_csv('data/news.csv', encoding='gb18030')
-    news = news.fillna('')
-    precessed = []
+    # v6
+    CPU_NUM = 4
+    pools = mp.Pool(CPU_NUM)
 
-    for i, row in tqdm(enumerate(news.iterrows())):
-        _, info = row
-        precessed.append(remove_non_char(remove_all_brackets(info['content'])))
+    # news = pd.read_csv('data/news.csv', encoding='gb18030') # v5
+    news = pd.read_csv('data/news.csv', encoding='gb18030', usecols=['content'], chunksize=CHUNK_SIZE)  # v5
 
-    news['content'] = precessed
+    funcs = []
+    for i, chunk in enumerate(news):
+        f = pools.apply_async(process_chunk, [chunk, i])
+        funcs.append(f)
 
-    all_length_medium = rank_length(news['content'].tolist())
+    for f in funcs:
+        f.get()
 
-    print(all_length_medium[:10])
-
-    with open('words_count.out', 'w', encoding='gb18030') as f:
-        for text in all_length_medium:
-            counter = Counter(text)
-            f.write('\n')
-            f.write(counter_to_text(counter))
+    # v6
+    # news = news.fillna('')
+    # precessed = []
+    #
+    # # v3
+    # functions = [
+    #     remove_all_brackets_re,
+    #     remove_non_char
+    # ]
+    #
+    # # v3
+    # # for i, row in tqdm(enumerate(news.iterrows())):
+    # #     _, info = row
+    # #     # precessed.append(remove_non_char(remove_all_brackets(info['content'])))  # v1
+    # #     # precessed.append(remove_non_char(remove_all_brackets_re(info['content'])))  # v2
+    # #     precessed.append(reduce_fn(functions, info['content']))  # v3
+    # # news['content'] = precessed
+    #
+    # # v4
+    # clean = clean_by_funcs(functions)
+    # news['content'].apply(lambda s: clean(s))
+    #
+    # all_length_medium = rank_length(news['content'].tolist())
+    #
+    # print(all_length_medium[:10])
+    #
+    # with open('words_count.out', 'w', encoding='gb18030') as f:
+    #     for text in all_length_medium:
+    #         counter = Counter(text)
+    #         f.write('\n')
+    #         # f.write(counter_to_text(counter)) # v1
+    #         f.write(counter_to_text_v2(counter)) # v3
